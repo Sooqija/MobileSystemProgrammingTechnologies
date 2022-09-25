3a209c0cebe5d0ae92b78d1ffe431dd861554595
PureEvil-UselessMan <OlyninAlex@gmail.com>
2022-09-25 15:56:04 +0300

    functionality

--- 02_Counter/app/src/main/java/com/example/a02_counter/MainActivity.kt
+++ 02_Counter/app/src/main/java/com/example/a02_counter/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.tan
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,7 +27,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
 
-        Score = findViewById(R.id.textView)
-
+        var counter = 0.0
+        Score = findViewById(R.id.textView2)
+        Refresh_Button = findViewById(R.id.RefreshButton)
+        Refresh_Button.setOnClickListener {
+            counter = 0.0
+            Score.text = counter.toString()
+        }
+        Increase_Button = findViewById(R.id.IncreaseButton)
+        Increase_Button.setOnClickListener {
+            counter++
+            Score.text = counter.toString()
+        }
+        Decrease_Button = findViewById(R.id.DecreaseButton)
+        Decrease_Button.setOnClickListener {
+            counter--
+            Score.text = counter.toString()
+        }
+        Power_Button = findViewById(R.id.PowerButton)
+        Power_Button.setOnClickListener {
+            counter = counter.pow(2)
+            Score.text = counter.toString()
+        }
+        Sin_Button = findViewById(R.id.SinButton)
+        Sin_Button.setOnClickListener {
+            counter = sin(counter)
+            Score.text = counter.toString()
+        }
+        Cos_Button = findViewById(R.id.CosButton)
+        Cos_Button.setOnClickListener {
+            counter = cos(counter)
+            Score.text = counter.toString()
+        }
+        Tan_Button = findViewById(R.id.TanButton)
+        Tan_Button.setOnClickListener {
+            counter = tan(counter)
+            Score.text = counter.toString()
+        }
+        Cot_Button = findViewById(R.id.CotButton)
+        Cot_Button.setOnClickListener {
+            counter = 1 / tan(counter)
+            Score.text = counter.toString()
+        }
     }
 }
