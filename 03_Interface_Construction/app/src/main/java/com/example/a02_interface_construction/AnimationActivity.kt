7772d43c0b46ad5a0ac8b4f20019890671bc7b25
PureEvil-UselessMan <OlyninAlex@gmail.com>
2022-10-03 19:19:36 +0300

    animation for square and rectangle

--- 03_Interface_Construction/app/src/main/java/com/example/a02_interface_construction/AnimationActivity.kt
+++ 03_Interface_Construction/app/src/main/java/com/example/a02_interface_construction/AnimationActivity.kt
@@ -3,6 +3,7 @@ package com.example.a02_interface_construction
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
@@ -12,6 +13,7 @@ class AnimationActivity: Activity() {
     private lateinit var NextPage_Button: Button
     private lateinit var PreviousPage_Button: Button
     private lateinit var Animated_Shape: TextView
+    private lateinit var Animated_Rectangle: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +21,43 @@ class AnimationActivity: Activity() {
 
         Animated_Shape = findViewById(R.id.BlackShape)
         Animated_Shape.setOnClickListener {
-            Animated_Shape.startAnimation(AnimationUtils.loadAnimation(this, R.anim.my_animation))
+            val animation = AnimationUtils.loadAnimation(this@AnimationActivity, R.anim.my_animation)
+            animation.setAnimationListener(object: Animation.AnimationListener {
+                    override fun onAnimationStart(animation: Animation?) {}
+                    override fun onAnimationRepeat(animation: Animation?) {}
+                    override fun onAnimationEnd(animation: Animation?) {
+                        Animated_Shape.startAnimation(AnimationUtils.loadAnimation(this@AnimationActivity, R.anim.my_animation_undo))
+                }
+            })
+            Animated_Shape.startAnimation(animation)
         }
 
+        Animated_Rectangle = findViewById(R.id.BlackRectangle)
+        Animated_Rectangle.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this@AnimationActivity, R.anim.rectangle_animation)
+            val animation_undo = AnimationUtils.loadAnimation(this@AnimationActivity, R.anim.rectangle_animation_undo)
+            animation.setAnimationListener(object: Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {
+                    Animated_Rectangle.text = "Превращаюсь в квадрат"
+                }
+                override fun onAnimationRepeat(animation: Animation?) {}
+                override fun onAnimationEnd(animation: Animation?) {
+//                    Animated_Rectangle.text = "Теперь я квадрат :("
+                    Thread.sleep(1000)
+                    Animated_Rectangle.startAnimation(animation_undo)
+                }
+            })
+            animation_undo.setAnimationListener(object: Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {
+                    Animated_Rectangle.text = "Превращаюсь в прямоульник"
+                }
+                override fun onAnimationRepeat(animation: Animation?) {}
+                override fun onAnimationEnd(animation: Animation?) {
+                    Animated_Rectangle.text = "Теперь я снова прямоугольник"
+                }
+            })
+            Animated_Rectangle.startAnimation(animation)
+        }
         NextPage_Button = findViewById(R.id.NextPage)
         NextPage_Button.setOnClickListener {
             val intent = Intent(this@AnimationActivity, DialogActivity::class.java)
